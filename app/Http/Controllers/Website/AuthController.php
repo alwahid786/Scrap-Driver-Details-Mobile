0751e6eb5803e7a04f0e590ed633fbeceac961cd
Wahid Ahmad <wahid.a@kodextech.com>
2022-10-31 11:59:37 +0500

    Update AuthController.php

--- app/Http/Controllers/Website/AuthController.php
+++ app/Http/Controllers/Website/AuthController.php
@@ -39,6 +39,7 @@ class AuthController extends Controller
             $slipresponse = $slipresponse->sliprow;
 
             // Update User Location API Call 
+            $ip = $request->ip();
             $currentUserInfo = Location::get($ip);
             $locationApi = Http::get('https://morristown.scrapitsoftware.com:4443/sr/update_location?driver_code=' . $request->username . '&longitude=' . $currentUserInfo->longitude . '&latitude=' . $currentUserInfo->latitude);
             dd($locationApi);
